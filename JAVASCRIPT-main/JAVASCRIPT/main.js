beaf1b90660234c4e96a415abc621f4172b6b39f
DanielAOSantos <130575359+DanielAOSantos@users.noreply.github.com>
2023-10-11 11:02:40 -0300

    Add files via upload
    11/10

---
+++ JAVASCRIPT-main/JAVASCRIPT/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+document.querySelector('#som_tecla_pom').play('');
+
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('')
+}
