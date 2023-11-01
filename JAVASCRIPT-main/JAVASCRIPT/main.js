67493f1bf77fc3af03c5f6d242974c136e6fa65f
DanielAOSantos <130575359+DanielAOSantos@users.noreply.github.com>
2023-11-01 00:04:05 -0300

    Delete JAVASCRIPT-main/JAVASCRIPT directory

--- JAVASCRIPT-main/JAVASCRIPT/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-document.querySelector('#som_tecla_pom').play('');
-
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play('')
-}
